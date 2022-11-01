61ed57fd8ac0d8fff32ff5fd4c6ab5bf93d96830
Gkhy <syan1022@naver.com>
2022-11-01 10:47:33 +0900

    user

---
+++ accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth import get_user_model
+
+# 유저폼 생성
+class UserForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ("username", "email")
+
+
+# 유저 변경 사항 폼 생성
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+        )
