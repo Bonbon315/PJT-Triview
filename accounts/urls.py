14d28d826bb69bd98b7a5ce1aed472c69863349f
Gkhy <syan1022@naver.com>
2022-11-01 14:17:39 +0900

    logout,link

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.log_in, name="login"),
+    path("logout/", views.log_out, name="logout"),
 ]
