083f7b4153ebf02c3d445d580f1895f190989e8d
Bonbon315 <n202u123@gmail.com>
2022-11-04 15:21:23 +0900

    sample data

--- triview/settings.py
+++ triview/settings.py
@@ -135,7 +135,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
